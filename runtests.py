6d7fcdccfd518ab5d8ccb7b0df2056646ce54137
Ricardo Silva <ricardo.silva@whitepropaganda.com.br>
2016-03-30 19:23:45 -0300

    Add TEMPLATE settings to runtests

--- runtests.py
+++ runtests.py
@@ -21,6 +21,22 @@ if not settings.configured:
 
             'django_handlebars',
         ],
+        TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                        'django.template.context_processors.i18n',
+                    ],
+                },
+            },
+        ]
     )
 
 try:
